271d7179ad47cd560587c3480a4569cfd4462bbd
Roman Kuznetsov <mmamano@inbox.ru>
2022-11-03 12:50:57 +0300

    fixed minor bug with cascade delete

--- src/main/resources/db/migration/V1__Init_DB.sql
+++ src/main/resources/db/migration/V1__Init_DB.sql
@@ -20,16 +20,16 @@ CREATE TABLE product_author(
     id int NOT NULL AUTO_INCREMENT,
     author_id int,
     PRINTED_PRODUCT int,
-    FOREIGN KEY (author_id) REFERENCES authors,
-    FOREIGN KEY (PRINTED_PRODUCT) REFERENCES printed_products,
+    FOREIGN KEY (author_id) REFERENCES authors ON DELETE CASCADE,
+    FOREIGN KEY (PRINTED_PRODUCT) REFERENCES printed_products ON DELETE CASCADE,
     PRIMARY KEY (id)
 );
 CREATE TABLE product_publisher(
     id int NOT NULL AUTO_INCREMENT,
     publisher_id int,
     PRINTED_PRODUCT int,
-    FOREIGN KEY (publisher_id) REFERENCES publishers,
-    FOREIGN KEY (PRINTED_PRODUCT) REFERENCES printed_products,
+    FOREIGN KEY (publisher_id) REFERENCES publishers ON DELETE CASCADE,
+    FOREIGN KEY (PRINTED_PRODUCT) REFERENCES printed_products ON DELETE CASCADE,
     PRIMARY KEY (id)
 );
 CREATE TABLE PRODUCT_TYPE(
@@ -38,4 +38,5 @@ CREATE TABLE PRODUCT_TYPE(
     primary key(id)
 );
 alter table printed_products
-    ADD FOREIGN KEY (type_id) REFERENCES PRODUCT_TYPE(id);
+    ADD FOREIGN KEY (type_id) REFERENCES PRODUCT_TYPE(id)
+    ON DELETE CASCADE;
